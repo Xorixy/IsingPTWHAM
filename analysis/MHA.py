a8a68633e8166c28dd5cd5cb10fa41f1a15718a8
Xorixy <soricib@gmail.com>
2024-11-26 17:06:17 +0100

    Fixed mha

--- analysis/MHA.py
+++ analysis/MHA.py
@@ -7,98 +7,74 @@ import scipy as scp
 
 filename = "../data/manual_spacing-3.h5"
 
-#size = 100*100
-
-time_series_start = 0
-time_series_end = 1000
-
-n_e_bins = 300
-n_m_bins = 300
+tol_sqr = 10e-12
+n_iter = 10000
 
 
-
-def mean_energy_function(betas, energy_series, log_zs, n_points):
-    raw_weights = raw_log_weights(betas, energy_series, log_zs)
-    sample_betas = np.linspace(betas[0], betas[-1], n_points)
-    sample_means = np.zeros(len(sample_betas))
-    for k in range(len(sample_betas)):
-        log_z = multiple_histogram_analysis(energy_series, sample_betas[k], 0, raw_weights)
-        sample_means[k] = multiple_histogram_analysis(energy_series, sample_betas[k], log_z, raw_weights, energy_series)
-        sample_means[k] = log_z
-    plt.plot(sample_betas, sample_means)
-    plt.plot(betas, log_zs, 'o')
+def get_mean_energies_sqr(betas, energy_series, raw_log_weights):
+    energies_sqr = np.zeros_like(betas)
+    for [k, beta] in enumerate(betas):
+        energies_sqr[k] = calculate_obs_avg(beta, energy_series, energy_series*energy_series, raw_log_weights)
+    return energies_sqr
+def get_mean_energies(betas, energy_series, raw_log_weights):
+    energies = np.zeros_like(betas)
+    for [k, beta] in enumerate(betas):
+        energies[k] = calculate_obs_avg(beta, energy_series, energy_series, raw_log_weights)
+    return energies
+def plot_heat_capacity(betas, energy_series, raw_log_weights, sample_betas = None):
+    energies = get_mean_energies(betas, energy_series, raw_log_weights)
+    energies_sqr = get_mean_energies_sqr(betas, energy_series, raw_log_weights)
+    plt.plot(betas, betas*betas*(energies_sqr - energies*energies))
+    if sample_betas is not None:
+        betas = sample_betas
+        energies = get_mean_energies(betas, energy_series, raw_log_weights)
+        energies_sqr = get_mean_energies_sqr(betas, energy_series, raw_log_weights)
+        plt.plot(betas, betas*betas*(energies_sqr - energies*energies), 'o')
     plt.show()
 
+def calculate_obs_avg(beta, energy_series, observable_series, raw_log_weights, log_z = None):
+    if log_z is None:
+        log_z = calculate_log_z(beta, energy_series, raw_log_weights)
+    obs_min = np.min(observable_series)
+    log_obs = np.log(observable_series - obs_min + 1)
+    logs_obs_mean = log_sum(log_obs - beta*energy_series - raw_log_weights) - log_z
+    return np.exp(logs_obs_mean) + obs_min - 1
 
-
-def multiple_histogram_analysis(energy_series, beta, log_z, raw_log_weights, observable_series = None):
-    if observable_series is None:
-        observable_series = np.ones(energy_series.shape)
-    log_observable_series = np.log(observable_series)
-    inv_log_weights = beta * energy_series + raw_log_weights
-    term_exponents = (log_observable_series - inv_log_weights).flatten()
-    largest_term = np.max(term_exponents)
-    term_exponents -= largest_term
-    term_exponents = np.delete(term_exponents, np.argmax(term_exponents))
-    sum_log = np.log1p(np.sum(np.exp(term_exponents)))
-    return -log_z + largest_term + sum_log
-
-def raw_log_weights(betas, energy_series, log_zs):
-    raw_log_weights = np.zeros(energy_series.shape)
-    for [i, series] in enumerate(energy_series):
-        for [j, energy] in enumerate(series):
-            raw_log_weights[i, j] = log_denominator_weight(0, energy, betas, len(series), log_zs)
-    return raw_log_weights
-
-tol_sqr = 10**-12
-def find_sim_log_partition_functions(betas, energy_series):
-    new_log_zs = np.zeros(len(betas))
-    print(new_log_zs)
-    delta_sqr = 1.0
-    n_iter = 10000
-    log_zs_iter_raw = np.zeros([n_iter, len(betas)])
-    n_iter_actual = n_iter
-    for _ in range (n_iter):
+def find_partition_logs(betas, energy_series):
+    new_log_zs = np.ones_like(betas)
+    for n in range(n_iter):
         old_log_zs = new_log_zs.copy()
-        for b in range(len(betas)):
-            #print(b)
-            new_log_zs[b] = approximate_log_quantity(betas[b], 0, betas, energy_series, old_log_zs)
+        raw_log_weights = calculate_raw_log_weights(betas, energy_series, old_log_zs)
+        for k in range(len(betas)):
+            new_log_zs[k] = calculate_log_z(betas[k], energy_series, raw_log_weights)
+        print(new_log_zs)
         deltas = np.exp(new_log_zs - old_log_zs) - 1
         delta_sqr = np.sum(deltas**2)
-        print(_)
-        print(new_log_zs)
-        print(deltas)
         print(delta_sqr)
-        log_zs_iter_raw[_] = new_log_zs.copy()
-        if delta_sqr < tol_sqr:
-            n_iter_actual = _ + 1
+        if (delta_sqr <= tol_sqr):
+            print(n_iter)
             break
-    log_zs_iter = log_zs_iter_raw[0:n_iter_actual-1]
-    print(1/np.arange(1, n_iter_actual + 1, 1))
-    for i in range(len(Ks)):
-        plt.plot(1/np.arange(1, n_iter_actual, 1), log_zs_iter[:,i], label=str(Ks[i]))
-    return new_log_zs
 
-def approximate_log_quantity(beta, log_z, betas, energy_series, log_zs, observable_series = None):
-    if observable_series is None:
-        observable_series = np.ones(energy_series.shape)
-    log_denominator_series = np.zeros(energy_series.shape)
+    return new_log_zs
+def calculate_log_z(beta, energy_series, raw_log_weights):
+    return log_sum((-beta*energy_series - raw_log_weights).flatten())
+
+def calculate_raw_log_weights(betas, energy_series, log_zs):
+    raw_log_weights = np.zeros_like(energy_series)
+    sizes = np.zeros(len(betas))
+    for i in range(len(betas)):
+        sizes[i] = len(energy_series[i])
     for [i, series] in enumerate(energy_series):
         for [j, energy] in enumerate(series):
-            log_denominator_series[i, j] = log_denominator_weight(beta, energy, betas, len(series), log_zs)
-    term_exponents = (np.log(observable_series) - log_denominator_series).flatten()
-    largest_term = np.max(term_exponents)
-    term_exponents -= largest_term
-    term_exponents = np.delete(term_exponents, np.argmax(term_exponents))
-    sum_log = np.log1p(np.sum(np.exp(term_exponents)))
-    return -log_z + largest_term + sum_log
-def log_denominator_weight(beta, energy, betas, sizes, log_zs):
-    term_exponents = np.log(sizes) - log_zs - betas*energy
-    largest_term = np.max(term_exponents)
-    term_exponents -= largest_term
-    term_exponents = np.delete(term_exponents, np.argmax(term_exponents))
-    sum_log = np.log1p(np.sum(np.exp(term_exponents)))
-    return beta*energy + largest_term + sum_log
+            raw_log_weights[i, j] = log_sum(np.log(sizes) - log_zs - betas*energy)
+
+    return raw_log_weights
+
+def log_sum(log_array):
+    maximum = np.max(log_array)
+    small_array = np.delete(log_array, np.argmax(log_array))
+    small_array -= maximum
+    return maximum + np.log1p(np.sum(np.exp(small_array)))
 
 
 f = h5.File(filename, 'r')
@@ -143,10 +119,14 @@ z_file = h5.File("manual_spacing_log_zs.h5", 'r')
 log_zs = z_file.get('log_zs')
 log_zs = log_zs[:]
 
+raw_weights = calculate_raw_log_weights(Ks, energy_time_series, log_zs)
+
+print(calculate_obs_avg(Ks[-1], energy_time_series, energy_time_series, raw_weights))
+
+plot_heat_capacity(np.linspace(Ks[0], Ks[-1], 200), energy_time_series, raw_weights, Ks)
 
 
-mean_energy_function(Ks, energy_time_series, log_zs, 40)
-
+#log_zs = find_partition_logs(Ks, energy_time_series)
 
 #log_zs = find_sim_log_partition_functions(Ks, energy_time_series)
 #out = h5.File("manual_spacing_log_zs.h5", 'w')
